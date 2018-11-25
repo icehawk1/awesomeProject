62490f0e9d1e09186e80718496a96607d9bbc282
Martin Haug <martinhaug@smart-mail.de>
2018-11-25 16:21:47 +0100

    + Kann ein level erzeugen

--- merklebaum/Merklebaum.go
+++ merklebaum/Merklebaum.go
@@ -1,33 +1,102 @@
 package merklebaum
 
-import "awesomeProject/blockchain"
+import (
+	"awesomeProject/blockchain"
+	"encoding/json"
+)
 
 type Merklebaum struct {
-	Hash string
-	left *Merklebaum
+	Hash  string
+	left  *Merklebaum
 	right *Merklebaum
+	elem  *blockchain.Hashable
+}
+
+func (self Merklebaum) ComputeHash() string {
+	if self.elem != nil {
+		return (*self.elem).ComputeHash()
+	} else {
+
+		// Prepend 0 to prevent second preimage attack
+		input := "0"
+		if self.left != nil {
+			input += self.left.Hash
+		}
+
+		if self.right != nil {
+			input += self.right.Hash
+		}
+
+		return blockchain.ComputeSha256Hex(input)
+	}
+}
+
+func (self Merklebaum) ComputeHashByte() []byte {
+	panic("implement me")
 }
 
 func CreateMerklebaum(content []blockchain.Hashable) Merklebaum {
-	return Merklebaum{}
+	if len(content) == 0 {
+		return Merklebaum{}
+	}
+
+	var leafs = make([]blockchain.Hashable, 0, len(content))
+	for i := 0; i < len(content); i++ {
+		leafs = append(leafs, Merklebaum{Hash: content[i].ComputeHash(), elem: &content[i]})
+	}
+
+	var bäume = make([]*Merklebaum, 0, len(leafs))
+	for len(bäume) > 1 {
+		bäume = createMerkleLevel(bäume)
+	}
+
+	return *bäume[0]
+}
+func createMerkleLevel(bäume []*Merklebaum) []*Merklebaum {
+
+	var result = make([]*Merklebaum, 0, len(bäume)/2+1)
+
+	// In case of odd number of trees, skip last tree for later
+	for i := 0; i+1 < len(bäume); i += 2 {
+		neuerbaum := Merklebaum{left: bäume[i], right: bäume[i+1]}
+		neuerbaum.Hash = neuerbaum.ComputeHash()
+		result = append(result, &neuerbaum)
+	}
+
+	if len(bäume)%2 == 1 {
+		result = append(result, &Merklebaum{left: bäume[len(bäume)-1]})
+	}
+	return result
 }
 
 func (self Merklebaum) IsValid() bool {
 	return false
 }
 
+func (self Merklebaum) IsLeaf() bool {
+	return self.left == nil && self.right == nil
+}
+
 func (self Merklebaum) HasNode(path []string) bool {
 	return false
 }
 
-func (self Merklebaum) GetLeafes() []blockchain.Hashable {
+func (self Merklebaum) GetElements() []blockchain.Hashable {
 	return []blockchain.Hashable{}
 }
 
 func (self Merklebaum) CreateSpvProof(leaf blockchain.Hashable) (proof []string, ok bool) {
-	return []string{},true
+	return []string{}, true
 }
 
 func (self Merklebaum) Contains(leaf blockchain.Hashable) bool {
 	return false
+}
+
+func (self *Merklebaum) MarshalJSON() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (self *Merklebaum) UnmarshalJSON(receivedData []byte) error {
+	return json.Unmarshal(receivedData, &self)
 }
--- merklebaum/Merklebaum_test.go
+++ merklebaum/Merklebaum_test.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"testing"
 )
+
 type TestContent struct {
 	text string
 }
+
 func (self TestContent) ComputeHash() string {
 	return blockchain.ComputeSha256Hex(self.text)
 }
@@ -17,11 +19,12 @@ func (self TestContent) ComputeHashByte() []byte {
 	return blockchain.ComputeSha256(self.text)
 }
 
-var contentlist = make([]blockchain.Hashable,0,10)
+var contentlist = make([]blockchain.Hashable, 0, 10)
 var baum Merklebaum
+
 func TestMain(m *testing.M) {
-	for i:=0; i<len(contentlist); i++ {
-		contentlist = append(contentlist, TestContent{fmt.Sprintf("Transaction %d",i)})
+	for i := 0; i < len(contentlist); i++ {
+		contentlist = append(contentlist, TestContent{fmt.Sprintf("Transaction %d", i)})
 	}
 	baum = CreateMerklebaum(contentlist)
 
@@ -34,18 +37,33 @@ func TestIsValid(t *testing.T) {
 
 func TestHasNode_valid(t *testing.T) {
 	proof, ok := baum.CreateSpvProof(TestContent{"Transaction 3"})
-	assert.True(t,ok)
+	assert.True(t, ok)
 	assert.True(t, baum.HasNode(proof))
 }
 
 func TestHasNode_invalid(t *testing.T) {
-	proof,ok := baum.CreateSpvProof(TestContent{"Transaction 666"})
-	assert.True(t,ok)
-	assert.True(t, baum.HasNode(proof))
+	proof, ok := baum.CreateSpvProof(TestContent{"Transaction 666"})
+	assert.True(t, ok)
+	assert.False(t, baum.HasNode(proof))
+}
+
+func TestCreateLevel(t *testing.T) {
+	bäume := []*Merklebaum{&Merklebaum{Hash: "abcd"}, &Merklebaum{Hash: "efgh"}, &Merklebaum{Hash: "ijkl"},
+		&Merklebaum{Hash: "mnop"}, &Merklebaum{Hash: "qrst"}}
+	assert.Equal(t, len(createMerkleLevel(bäume[:3])), 2)
+	assert.Equal(t, len(createMerkleLevel(bäume[:4])), 2)
+	assert.Equal(t, len(createMerkleLevel(bäume[:5])), 3)
+
+	assert.Equal(t, createMerkleLevel(bäume)[1].Hash, blockchain.ComputeSha256Hex("0ijklmnop"))
+}
+
+func TestCreateMerklebaum_oddlen(t *testing.T) {
+	actual := CreateMerklebaum(contentlist[:3])
+	assert.Equal(t, len(actual.GetElements()), 3)
 }
 
 func TestGetLeafes(t *testing.T) {
-	actual := baum.GetLeafes()
+	actual := baum.GetElements()
 	assert.Equal(t, len(actual), len(contentlist))
 	assert.Equal(t, actual[3].ComputeHash(), blockchain.ComputeSha256("Transaction 3"))
 }
